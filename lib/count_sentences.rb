2061c85a7a31688c572ecc9f3823113decea91e0
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-13 18:57:39 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,6 @@ class String
   end
 
   def count_sentences
-    self.split(Regexp.union(".", "?","!")).reject{|e| e == ""}.length
+    self.split(Regexp.union(".", "?","!")).reject(&:empty?).length
   end
 end
